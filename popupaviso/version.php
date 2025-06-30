6a14c137c93b4a2ff628fa313188ef0b14575b22
moysesemanuel-dev <moyses.emanuel@mletras.com.br>
2025-06-30 16:25:25 -0300

    Update version.php

--- popupaviso/version.php
+++ popupaviso/version.php
@@ -3,6 +3,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_popupaviso';
 $plugin->version = 2025063000;
-$plugin->requires = 2022041900; // Moodle 4.0 ou superior
+$plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = '1.0';
